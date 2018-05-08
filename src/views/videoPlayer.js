171952430a1bb4091cd6ba83dc29463fcee139c5
grantyang <grantyang1@gmail.com>
2018-05-08 15:27:26 -0700

    autoplay functionality and expand details

--- src/views/videoPlayer.js
+++ src/views/videoPlayer.js
@@ -1,15 +1,57 @@
 var VideoPlayerView = Backbone.View.extend({
 
   initialize: function () {
+    this.currentVideo;
+    this.autoplayValue = 0;
     this.collection.on('select', (model) => this.render(model));
     this.collection.on('sync', () => this.collection.models[0].select());
+    this.$el.on('click', '#expandBtn', () => { this.expand(); });
+    this.$el.on('change', '.autoplaySlider', () => {
+      this.autoplayValue = this.autoplayValue === 0 ? 1 : 0;
+      this.render(this.currentVideo);
+    });
+  },
 
+  expand: function () {
+    var currentVideo = this.currentVideo;
+    var video = this;
+    Backbone.ajax({
+      url: 'https://www.googleapis.com/youtube/v3/videos',
+      type: 'GET',
+      data: {
+        'id': currentVideo.id,
+        'part': 'snippet',
+        'key': window.YOUTUBE_API_KEY,
+      },
+      contentType: 'application/json',
+      success: function (response) {
+        console.log('expand success!');
+        console.log(response);
+        video.render(response.items[0]);
+      },
+      error: function () {
+        console.log('GET failure!');
+      }
+    });
   },
 
 
   render: function (model) {
     if (model !== undefined) {
-      this.$el.html(this.template(model.attributes));
+      this.currentVideo = model;
+      if (model.attributes !== undefined) {
+        this.$el.html(this.template(
+          Object.assign({
+            autoplayValue: this.autoplayValue,
+            checked: this.autoplayValue ? 'checked' : ''
+          }, model.attributes)));
+      } else {
+        this.$el.html(this.template(
+          Object.assign({
+            autoplayValue: this.autoplayValue,
+            checked: this.autoplayValue ? 'checked' : ''
+          }, model)));
+      }
     }
     return this;
   },
