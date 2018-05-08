77426a359a0c1693b16c02d9b98eccc64e8939c9
grantyang <grantyang1@gmail.com>
2018-05-07 17:54:37 -0700

    tests complete up until VideoListView

--- src/views/videoList.js
+++ src/views/videoList.js
@@ -1,7 +1,18 @@
 var VideoListView = Backbone.View.extend({
 
+  initialize: function () {
+    // this.collection.on('change', this.render, this);
+    // this.render();
 
-  render: function() {
+  },
+
+  render: function () {
+    if (this.collection !== undefined) {
+      this.collection.forEach((video) => {
+        console.log(video);
+        new VideoListEntryView({ model: video });
+      });
+    }
     this.$el.children().detach();
     this.$el.html(this.template());
     return this;
